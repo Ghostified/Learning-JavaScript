5b36d870371aee5778c6d73bf31419e061c4257f
Ghostified <bransonallan@gmail.com>
2024-06-29 23:07:24 +0300

    operators

--- Variables/variablesInJS.js
+++ Variables/variablesInJS.js
@@ -3,9 +3,9 @@ let - can be changed , the variable can be changed later on
 const -is constant
 */
 
-let a = 10; 
+let a = 10;
 const b = 20;
- let c = b + a;
+let c = b + a;
 
 console.log(c)
 
@@ -16,26 +16,26 @@ let studentOne = 'Branson';
 
 //Objects
 let allStudents = {
-    studentA : "Hermione ",
-    studentB : "Ghost",
-    studentC : "Ron",
+    studentA: "Hermione ",
+    studentB: "Ghost",
+    studentC: "Ron",
 }
 
 //Arays 
-const arrayExample = ["Hermione", "Jame", "Potter","Malfoy"];
+const arrayExample = ["Hermione", "Jame", "Potter", "Malfoy"];
 
 //Boolean  Expressions return either a true or a false
 //ES6 functions => is an arrow function
 
-const passedStudent =  () => {
+const passedStudent = () => {
     const passed = {
-        studentOne : "James",
-        studentTwo : "Granger",
+        studentOne: "James",
+        studentTwo: "Granger",
     };
 
     const failedStudent = {
-        studentZ : "Crabbe",
-        studentX : "Goyle"
+        studentZ: "Crabbe",
+        studentX: "Goyle"
     }
     if (studentOne) {
         console.log(passed)
@@ -44,5 +44,64 @@ const passedStudent =  () => {
     }
 }
 
+console.log(arrayExample)
 
-//operators 
+
+/*
+Data types - undefined, null, boolean , string, symbol , number, object 
+*/
+
+var a1;
+var b1 = 2;
+console.log(a1);
+console.log(b1);
+
+var a2 = 9;
+var b2 = 10;
+var c3 = "Welcome to hogwarts"
+
+a2 = a + 1;
+b2 = b2 + 5;
+c3 = c3 + " string";
+
+console.log(a2);
+console.log(b2);
+console.log(c3);
+
+
+var sum = 10 + 10;
+var subtract  = 45 -10;
+var divide = 30 /3;
+var multiply = 5 * 5;
+
+console.log (sum);
+console.log (subtract)
+console.log (divide);
+console.log(multiply)
+
+//increment
+var myvar = 87;
+myvar = myvar + 1;
+myvar++;
+console.log(myvar);
+
+var myInt = 89;
+myInt = myInt -1;
+myInt--;
+console.log(myInt);
+
+//Floats
+var decimals = 5.7;
+
+var product = 3.6 * 5.9;
+console.log(product)
+
+var division = 7.9 / 5.6;
+console.log (division);
+
+var remainder ;
+remainder = 90 % 8;
+console.log(remainder);
+
+
+
